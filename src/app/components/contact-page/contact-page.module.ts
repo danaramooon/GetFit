f6a3fc3bda4bf651e278f3eb3fc42a5cece27ce7
danaramooon <danaramooon@gmail.com>
2019-03-19 21:20:02 +0600

    contact page updated

--- src/app/components/contact-page/contact-page.module.ts
+++ src/app/components/contact-page/contact-page.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ContactPageComponent } from './contact-page.component';
 import { MapComponent } from './map/map.component';
 import { BannerAreaModule } from '../banner-area/banner-area.module';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     ContactPageComponent, 
-    MapComponent,
+    MapComponent, FormComponent,
   ],
   imports: [
     CommonModule,
