ee8e29d7fc76f492628c6bc2d0c9b412c5d59746
ZhalgasZhumabayev <danaramooon@gmail.com>
2019-03-20 02:35:26 +0600

    banner-area fixed

--- src/app/components/banner-area/banner-area.component.ts
+++ src/app/components/banner-area/banner-area.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit,Input } from '@angular/core';
 export class BannerAreaComponent implements OnInit {
   @Input() imgPath = '';
   @Input() title = '';
-  @Input() title1 = '';
+  @Input() title1 = 'Лучший сервис по доставке правильного и сбалансированного питания в вашем городе!';
+  @Input() btnText = 'Выбрать программу';
   constructor() { }
   
   ngOnInit() {
